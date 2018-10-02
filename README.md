1b746acc0cdddee9c8d72fd349a4e921ffdfd7d7
Yuichi Saito <munisystem@gmail.com>
2018-10-02 11:47:17 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# grpc-health-check
+
+This repository is an example how implement health check in gRPC server with Go.
+
+## References
+
+- https://kubernetes.io/blog/2018/10/01/health-checking-grpc-servers-on-kubernetes/
+- https://godoc.org/google.golang.org/grpc/health/grpc_health_v1
+- https://github.com/grpc-ecosystem/grpc-health-probe/
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct{}
+
+func (s *server) Check(ctx context.Context, in *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
+	return &grpc_health_v1.HealthCheckResponse{
+		Status: grpc_health_v1.HealthCheckResponse_SERVING,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	grpc_health_v1.RegisterHealthServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
