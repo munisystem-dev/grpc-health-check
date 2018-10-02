a09632b19ed6fb0a1e4680cfcd52dc29882fffef
Yuichi Saito <munisystem@gmail.com>
2018-10-02 13:34:14 +0900

    Use official initializer

--- main.go
+++ main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
 )
-
-type server struct{}
-
-func (s *server) Check(ctx context.Context, in *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
-	return &grpc_health_v1.HealthCheckResponse{
-		Status: grpc_health_v1.HealthCheckResponse_SERVING,
-	}, nil
-}
 
 func main() {
 	lis, err := net.Listen("tcp", ":8080")
@@ -23,7 +15,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	grpc_health_v1.RegisterHealthServer(s, &server{})
+	grpc_health_v1.RegisterHealthServer(s, health.NewServer())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
